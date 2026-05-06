4ae36ad8751d4d875389eee48c814d43aa513325
nevermind6767 <fathima20251544@miis.ae>
2026-05-06 12:54:29 +0400

    Strmlit linear  .py

--- Strmlit linear  .py
+++ Strmlit linear  .py
@@ -27,7 +27,7 @@ hours = st.number_input("Hours Studied:", min_value=0.0, step=0.1)
 # Predict Button
 if st.button("Predict Score"):
     predicted_score = model.predict([[hours]])[0]
-    st.success(f"Predicted Score: {float(predicted_score):.2f}")
+    st.success(f"Predicted Score: {predicted_score[0]:.2f}")
 
 # Show Sample Data
 st.write("### 📊 Sample Training Data")
